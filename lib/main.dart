275f3fcf965a9eb7d681321031e3cb6673459063
yuri.pereira <yuri.pereira@ambevtech.com.br>
2021-03-12 17:42:57 -0300

    aula 119 finalizada. lendo os dados do banco de dados

--- lib/main.dart
+++ lib/main.dart
@@ -3,23 +3,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main()async{
   runApp(MyApp());
-
-  QuerySnapshot snapshot = await Firestore.instance.collection('mensagens').getDocuments();
-  snapshot.documents.forEach((d) {
-    print(d.data);
-  });
-  /**
-   *
-   * Para escrever os dados no banco de dados.
-      em .document() se chamar o metodo sem passar o parametro ele cria o id
-   * automaticamente do banco de dados.
-   *
-  Firestore.instance.collection('mensagens').document().collection('arquivos')
-      .document().setData({
-    'from' : 'Yuri',
-      'texto' : 'Tudo Bem?'
-  });
-   */
 }
 
 
