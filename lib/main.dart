1daae2d3fd081350bb3576badb62099a77176f6b
yuri.pereira <yuri.pereira@ambevtech.com.br>
2021-03-12 16:56:33 -0300

    aula 117 finalizada. Configuração do projeto com o Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
+  Firestore.instance.collection("col").document("doc").setData({"texto" : "Yuri"});
 }
 
 class MyApp extends StatelessWidget {
